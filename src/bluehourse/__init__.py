089051e1381a3d40ff8898ca18918fcfc2b157b0
Maksym Shalenyi <supamaxy@gmail.com>
2016-07-18 19:23:45 +0300

    added some hooks

--- src/bluehourse/__init__.py
+++ src/bluehourse/__init__.py
@@ -1,14 +1,15 @@
 from pyramid.config import Configurator
+from pyramid.traversal import DefaultRootFactory
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    config = Configurator(settings=settings, root_factory=DefaultRootFactory)
     config.include('pyramid_jinja2')
-    config.include('.admin')
     config.include('.models')
     config.include('.routes')
     config.include('.security')
+    config.include('.admin')
     config.scan()
     return config.make_wsgi_app()
--- src/bluehourse/admin.py
+++ src/bluehourse/admin.py
@@ -1,8 +1,19 @@
 from bluehourse.models import Page, User
 
+from pyramid.security import Allow, Everyone
+from pyramid_sacrud import PYRAMID_SACRUD_HOME
+from pyramid.interfaces import IRootFactory
+
+
+def pach_root_acl_factory(root_factory):
+    root_factory.__acl__ = [
+        (Allow, Everyone, PYRAMID_SACRUD_HOME),
+    ]
+
 
 def includeme(config):
-    # import pdb; pdb.set_trace()
+    config.commit()
+    pach_root_acl_factory(config.registry.getUtility(IRootFactory))
     config.include('ps_alchemy')
     config.include('pyramid_sacrud')
     settings = config.registry.settings
